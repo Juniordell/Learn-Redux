06f6d8741b6c99ed1bc41ef1150632e3e9313193
Nelson Jr <juniordell123@gmail.com>
2020-09-15 22:39:17 -0300

    update the Animes page

--- src/pages/Animes/index.tsx
+++ src/pages/Animes/index.tsx
@@ -1,14 +1,43 @@
 import React, { useEffect, useState } from 'react'
 
-import api from '../../api'
-
-import Loader from 'react-loader-spinner'
-
 import './styles.css'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import { useFetch } from '../../hooks/useFetch'
+
+const listOfIds: any = []
 
 const Animes: React.FC = () => {
-  return <div></div>
+  const { data } = useFetch('animes')
+  const [id, setId] = useState(listOfIds[~~(Math.random() * listOfIds.length)])
+
+  useEffect(() => {
+    data?.data.map((anime: any) => {
+      listOfIds.push(anime.id)
+    })
+
+    setId(listOfIds[~~(Math.random() * listOfIds.length)])
+  }, [data])
+
+  function changeAnime() {
+    setId(listOfIds[~~(Math.random() * listOfIds.length)])
+  }
+
+  return (
+    <div className='animes_container'>
+      <h1>What Anime should i watch?</h1>
+
+      {data?.data.map(
+        (anime: any) =>
+          anime.id === id && (
+            <div className='animes_content'>
+              <h1>{anime.name}</h1>
+              <img key={anime.id} src={anime.image} alt='sad' />
+              <button onClick={changeAnime}>Random</button>
+            </div>
+          ),
+      )}
+    </div>
+  )
 }
 
 export default Animes
